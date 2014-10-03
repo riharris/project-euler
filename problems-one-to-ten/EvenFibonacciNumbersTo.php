a0eed69a407a4ca0e1e408ead482cc14efab6b1e
riharris <riharrison@estee-lauder.co.uk>
2014-10-03 12:05:29 +0100

    Worked with fibonacci numbers

---
+++ problems-one-to-ten/EvenFibonacciNumbersTo.php
@@ -0,0 +1,57 @@
+<?php
+include_once 'AbstractIterator.php';
+include_once 'Number.php';
+
+/**
+ * Contains the EvenFibonacciNumbersTo class
+ *
+ * @author Richard Harrison
+ * @since 3 Oct 2014
+ */
+class EvenFibonacciNumbersTo extends AbstractIterator
+{
+
+    /**
+     *
+     * @param int $limit            
+     */
+    public function __construct($limit)
+    {
+        /**
+         *
+         * @var array $values
+         */
+        $values = array();
+        
+        /**
+         *
+         * @var array $odds
+         */
+        $odds = array(
+            1,
+            1
+        );
+        
+        while (array_sum($odds) <= $limit) {
+            
+            $values[] = array_sum($odds);
+            
+            $odds = $this->getNextTwoOddNumbersAssumingOOEOOESequence($odds);
+        }
+        
+        $this->iterator = new ArrayIterator($values);
+    }
+
+    /**
+     *
+     * @param array $odds            
+     * @return array
+     */
+    protected function getNextTwoOddNumbersAssumingOOEOOESequence(array $odds)
+    {
+        return array(
+            end($odds) + array_sum($odds),
+            end($odds) + array_sum($odds) + array_sum($odds)
+        );
+    }
+}
---
+++ problems-one-to-ten/FibonacciNumbersTo.php
@@ -0,0 +1,44 @@
+<?php
+include_once 'AbstractIterator.php';
+
+/**
+ * Contains the FibonacciNumbersTo class
+ *
+ * @author Richard Harrison
+ * @since 3 Oct 2014
+ */
+class FibonacciNumbersTo extends AbstractIterator
+{
+
+    /**
+     * @param int $limit
+     */
+    public function __construct($limit)
+    {
+        /**
+         *
+         * @var array
+         */
+        $values = array(
+            1,
+            1
+        );
+        
+        while ($this->getNextElement($values) <= $limit) {
+            
+            $values[] = $this->getNextElement($values);
+        }
+        
+        $this->iterator = new ArrayIterator($values);
+    }
+
+    /**
+     *
+     * @param array $values            
+     * @return int
+     */
+    protected function getNextElement(array $values)
+    {
+        return array_sum(array_slice($values, - 2));
+    }
+}
