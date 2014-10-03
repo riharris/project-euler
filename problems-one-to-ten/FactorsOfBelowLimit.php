5ffb97cb9a2dd123d268f232ed856166a14c75a8
Richard <richard.arthur.harrison@googlemail.com>
2014-10-03 19:30:36 +0100

    Worked with smallest multiple test

--- problems-one-to-ten/FactorsOfBelowLimit.php
+++ problems-one-to-ten/FactorsOfBelowLimit.php
@@ -1,4 +1,5 @@
 <?php
+include_once 'AbstractFactorsOf.php';
 include_once 'Number.php';
 
 /**
--- problems-one-to-ten/SmallestMultipleTest.php
+++ problems-one-to-ten/SmallestMultipleTest.php
@@ -1,7 +1,7 @@
 <?php
-
 include_once 'Number.php';
 include_once 'FactorMatcher.php';
+include_once 'FactorsOfBelowLimit.php';
 
 /**
  * Contains the SmallestMultipleTest class
@@ -10,29 +10,62 @@ include_once 'FactorMatcher.php';
  * @copyright Estee Lauder Companies Ltd. 2013
  * @since 2 Nov 2013
  */
-class SmallestMultipleTest extends PHPUnit_Framework_TestCase {
+class SmallestMultipleTest extends PHPUnit_Framework_TestCase
+{
 
-	public function testConfirmFactorsForKnownValue(){
+    public function testConfirmFactorsForGivenKnownValue()
+    {
+        $this->assertSame(array(
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9',
+            '10'
+        ), iterator_to_array(new FactorsOfBelowLimit(new Number(2520), 10)));
+    }
 
-		$fixture = new Number(2520);
-
-		$this->assertSame(array_map('strval', range(2, 10)), $fixture->setLargestFactorSought(10)
-			->getFactors());
-	}
-
-	public function testResultForKnownPattern(){
-
-		$fixture = new FactorMatcher();
-
-		$this->assertSame('2520', $fixture->setPattern(range(2, 10))
-			->getFirstInstance());
-	}
-
-	public function testResultForUnknownPattern(){
-
-		$fixture = new FactorMatcher();
-
-		$this->assertSame('232792560', $fixture->setPattern(range(2, 20))
-			->getFirstInstance());
-	}
+    public function testConfirmFactorsForLargerKnownValue()
+    {
+        $this->assertSame(array(
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9',
+            '10',
+            '11',
+            '12',
+            '13',
+            '14',
+            '15',
+            '16',
+            '17',
+            '18',
+            '19',
+            '20'
+        ), iterator_to_array(new FactorsOfBelowLimit(new Number(20 * 19 * 18 * 17 * 16 * 15 * 14 * 13 * 12 * 11), 20)));
+    }
+    
+    // public function testResultForKnownPattern(){
+    
+    // $fixture = new FactorMatcher();
+    
+    // $this->assertSame('2520', $fixture->setPattern(range(2, 10))
+    // ->getFirstInstance());
+    // }
+    
+    // public function testResultForUnknownPattern(){
+    
+    // $fixture = new FactorMatcher();
+    
+    // $this->assertSame('232792560', $fixture->setPattern(range(2, 20))
+    // ->getFirstInstance());
+    // }
 }
