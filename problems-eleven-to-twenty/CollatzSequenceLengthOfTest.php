11fd00df5bec1a24f827b917122aab3dd5f3ff13
riharris <riharrison@estee-lauder.co.uk>
2014-10-14 11:12:00 +0100

    Added collatz sequence generator

---
+++ problems-eleven-to-twenty/CollatzSequenceLengthOfTest.php
@@ -0,0 +1,18 @@
+<?php
+include_once 'CollatzSequenceLengthOf.php';
+include_once 'Number.php';
+
+/**
+ * Contains the CollatzSequenceLengthOfTest class
+ *
+ * @author Richard Harrison
+ * @since 14 Oct 2014
+ */
+class CollatzSequenceLengthOfTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testConfirmLengthMatchesExpectedForKnownSequence()
+    {
+        $this->assertSame('', strval(new CollatzSequenceLengthOf(new Number(13))));
+    }
+}
