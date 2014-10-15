984cbe129d53b3fb68d23c0250893ce133d740ec
riharris <riharrison@estee-lauder.co.uk>
2014-10-15 15:41:08 +0100

    Worked with lattice path problem

--- problems-eleven-to-twenty/LatticePathCountTest.php
+++ problems-eleven-to-twenty/LatticePathCountTest.php
@@ -20,7 +20,7 @@ class LatticePathCountTest extends PHPUnit_Framework_TestCase
     
     public function testConfirmOutputMatchesExpectedForUnknownProblem()
     {
-        $fixture = new LatticePathCount(5);
+        $fixture = new LatticePathCount(20);
     
         $this->assertSame(6, count(iterator_to_array($fixture)));
     }
